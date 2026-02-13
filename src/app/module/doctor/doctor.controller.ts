4da7975e5c5a6fe71d247d1f2e3566aad8595a34
Ahsanul Haque <ahsanul0185@gmail.com>
2026-02-13 13:05:26 +0600

    doctors crud done

--- src/app/module/doctor/doctor.controller.ts
+++ src/app/module/doctor/doctor.controller.ts
@@ -35,7 +35,40 @@ const getDoctorById = catchAsync(
     }
 )
 
+const updateDoctor = catchAsync(
+    async (req: Request, res: Response) => {
+        const { id } = req.params;
+        const payload = req.body;
+
+        const updatedDoctor = await doctorService.updateDoctor(id as string, payload);
+
+        sendResponse(res, {
+            httpStatusCode: status.OK,
+            success: true,
+            message: "Doctor updated successfully",
+            data: updatedDoctor,
+        })
+    }
+)
+
+const deleteDoctor = catchAsync(
+    async (req: Request, res: Response) => {
+        const { id } = req.params;
+
+        const result = await doctorService.deleteDoctor(id as string);
+
+        sendResponse(res, {
+            httpStatusCode: status.OK,
+            success: true,
+            message: "Doctor deleted successfully",
+            data: result,
+        })
+    }
+)
+
 export const doctorController = {
     getAllDoctors,
-    getDoctorById
+    getDoctorById,
+    updateDoctor,
+    deleteDoctor
 }
